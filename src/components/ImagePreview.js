f5055c64eaf237c0eed51e0ae2340d594fd8ae91
Samuel Brandão <gb.samuel@gmail.com>
2016-01-16 17:01:39 +0100

    added propTypes

--- src/components/ImagePreview.js
+++ src/components/ImagePreview.js
@@ -1,13 +1,52 @@
 import React from 'react'
 
-export default class ImagePreview extends React.Component {
+class ImagePreview extends React.Component {
   constructor () {
     super()
+
+    this.state = {
+      selected: false
+    }
   }
+
   render () {
+    const toggleSelection = this.toggleSelection.bind(this)
+
     return (
-      <div> Hello
+      <div>
+        <img
+          src={this.props.src}
+          style={this.getImageStyle()}
+          onClick={toggleSelection}
+        />
       </div>
     )
   }
+
+  getImageStyle () {
+    const defaultBorder = '2px solid blue'
+    const highlightedBorder = 'red'
+
+    const imageStyle = {
+      border: defaultBorder,
+      borderColor: 'transparent',
+      height: '100px'
+    }
+
+    if (this.state.selected) {
+      imageStyle.borderColor = highlightedBorder
+    }
+
+    return imageStyle
+  }
+
+  toggleSelection () {
+    this.setState({selected: !this.state.selected})
+  }
 }
+
+ImagePreview.propTypes = {
+  src: React.PropTypes.string.isRequired
+}
+
+export default ImagePreview
--- src/main.js
+++ src/main.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom'
 import React from 'react'
 
 ReactDOM.render(
-  <ImagePreview />,
+  <ImagePreview src='/home/sam/projects/hello-electron/samples/cat-treats.jpg' />,
   document.getElementById('image-preview')
 )
